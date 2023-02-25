Ramsesdb/gexu-legacy-fork

Replace Kotlin synthetic views in viewholders

app/src/main/java/eu/kanade/tachiyomi/ui/browse/migration/sources/SelectionHeader.kt
@@ -5,9 +5,9 @@ import androidx.recyclerview.widget.RecyclerView
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.AbstractHeaderItem
 import eu.davidea.flexibleadapter.items.IFlexible
+import eu.davidea.viewholders.FlexibleViewHolder
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.ui.base.holder.BaseFlexibleViewHolder
-import kotlinx.android.synthetic.main.source_main_controller_card_header.title
+import eu.kanade.tachiyomi.databinding.SourceMainControllerCardHeaderBinding
 
 /**
  * Item that contains the selection header.
@@ -43,9 +43,12 @@ class SelectionHeader : AbstractHeaderItem<SelectionHeader.Holder>() {
         // Intentionally empty
     }
 
-    class Holder(view: View, adapter: FlexibleAdapter<*>) : BaseFlexibleViewHolder(view, adapter) {
+    class Holder(view: View, adapter: FlexibleAdapter<*>) : FlexibleViewHolder(view, adapter) {
+
+        private val binding = SourceMainControllerCardHeaderBinding.bind(view)
+
         init {
-            title.text = view.context.getString(R.string.migration_selection_prompt)
+            binding.title.text = view.context.getString(R.string.migration_selection_prompt)
         }
     }
 

app/src/main/java/eu/kanade/tachiyomi/ui/recent/history/HistoryHolder.kt
@@ -2,18 +2,13 @@ package eu.kanade.tachiyomi.ui.recent.history
 
 import android.view.View
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import eu.davidea.viewholders.FlexibleViewHolder
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.MangaChapterHistory
 import eu.kanade.tachiyomi.data.glide.GlideApp
 import eu.kanade.tachiyomi.data.glide.toMangaThumbnail
-import eu.kanade.tachiyomi.ui.base.holder.BaseFlexibleViewHolder
+import eu.kanade.tachiyomi.databinding.HistoryItemBinding
 import eu.kanade.tachiyomi.util.lang.toTimestampString
-import kotlinx.android.synthetic.main.history_item.cover
-import kotlinx.android.synthetic.main.history_item.holder
-import kotlinx.android.synthetic.main.history_item.manga_subtitle
-import kotlinx.android.synthetic.main.history_item.manga_title
-import kotlinx.android.synthetic.main.history_item.remove
-import kotlinx.android.synthetic.main.history_item.resume
 import java.util.Date
 
 /**
@@ -28,18 +23,20 @@ import java.util.Date
 class HistoryHolder(
     view: View,
     val adapter: HistoryAdapter
-) : BaseFlexibleViewHolder(view, adapter) {
+) : FlexibleViewHolder(view, adapter) {
+
+    private val binding = HistoryItemBinding.bind(view)
 
     init {
-        holder.setOnClickListener {
+        binding.holder.setOnClickListener {
             adapter.itemClickListener.onItemClick(bindingAdapterPosition)
         }
 
-        remove.setOnClickListener {
+        binding.remove.setOnClickListener {
             adapter.removeClickListener.onRemoveClick(bindingAdapterPosition)
         }
 
-        resume.setOnClickListener {
+        binding.resume.setOnClickListener {
             adapter.resumeClickListener.onResumeClick(bindingAdapterPosition)
         }
     }
@@ -54,26 +51,26 @@ class HistoryHolder(
         val (manga, chapter, history) = item
 
         // Set manga title
-        manga_title.text = manga.title
+        binding.mangaTitle.text = manga.title
 
         // Set chapter number + timestamp
         if (chapter.chapter_number > -1f) {
             val formattedNumber = adapter.decimalFormat.format(chapter.chapter_number.toDouble())
-            manga_subtitle.text = itemView.context.getString(
+            binding.mangaSubtitle.text = itemView.context.getString(
                 R.string.recent_manga_time,
                 formattedNumber,
                 Date(history.last_read).toTimestampString()
             )
         } else {
-            manga_subtitle.text = Date(history.last_read).toTimestampString()
+            binding.mangaSubtitle.text = Date(history.last_read).toTimestampString()
         }
 
         // Set cover
-        GlideApp.with(itemView.context).clear(cover)
+        GlideApp.with(itemView.context).clear(binding.cover)
         GlideApp.with(itemView.context)
             .load(manga.toMangaThumbnail())
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
             .centerCrop()
-            .into(cover)
+            .into(binding.cover)
     }
 }

app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/globalsearch/GlobalSearchCardHolder.kt
@@ -2,18 +2,17 @@ package eu.kanade.tachiyomi.ui.browse.source.globalsearch
 
 import android.view.View
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import eu.davidea.viewholders.FlexibleViewHolder
 import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.data.glide.GlideApp
 import eu.kanade.tachiyomi.data.glide.toMangaThumbnail
-import eu.kanade.tachiyomi.ui.base.holder.BaseFlexibleViewHolder
+import eu.kanade.tachiyomi.databinding.GlobalSearchControllerCardItemBinding
 import eu.kanade.tachiyomi.widget.StateImageViewTarget
-import kotlinx.android.synthetic.main.global_search_controller_card_item.card
-import kotlinx.android.synthetic.main.global_search_controller_card_item.cover
-import kotlinx.android.synthetic.main.global_search_controller_card_item.progress
-import kotlinx.android.synthetic.main.global_search_controller_card_item.title
 
 class GlobalSearchCardHolder(view: View, adapter: GlobalSearchCardAdapter) :
-    BaseFlexibleViewHolder(view, adapter) {
+    FlexibleViewHolder(view, adapter) {
+
+    private val binding = GlobalSearchControllerCardItemBinding.bind(view)
 
     init {
         // Call onMangaClickListener when item is pressed.
@@ -33,17 +32,17 @@ class GlobalSearchCardHolder(view: View, adapter: GlobalSearchCardAdapter) :
     }
 
     fun bind(manga: Manga) {
-        card.clipToOutline = true
+        binding.card.clipToOutline = true
 
-        title.text = manga.title
+        binding.title.text = manga.title
         // Set alpha of thumbnail.
-        cover.alpha = if (manga.favorite) 0.3f else 1.0f
+        binding.cover.alpha = if (manga.favorite) 0.3f else 1.0f
 
         setImage(manga)
     }
 
     fun setImage(manga: Manga) {
-        GlideApp.with(itemView.context).clear(cover)
+        GlideApp.with(itemView.context).clear(binding.cover)
         if (!manga.thumbnail_url.isNullOrEmpty()) {
             GlideApp.with(itemView.context)
                 .load(manga.toMangaThumbnail())
@@ -51,7 +50,7 @@ class GlobalSearchCardHolder(view: View, adapter: GlobalSearchCardAdapter) :
                 .centerCrop()
                 .skipMemoryCache(true)
                 .placeholder(android.R.color.transparent)
-                .into(StateImageViewTarget(cover, progress))
+                .into(StateImageViewTarget(binding.cover, binding.progress))
         }
     }
 }

az4521/TachiyomiAZ

app/src/main/java/eu/kanade/tachiyomi/ui/migration/SelectionHeader.kt
@@ -5,9 +5,9 @@ import androidx.recyclerview.widget.RecyclerView
 import eu.davidea.flexibleadapter.FlexibleAdapter
 import eu.davidea.flexibleadapter.items.AbstractHeaderItem
 import eu.davidea.flexibleadapter.items.IFlexible
+import eu.davidea.viewholders.FlexibleViewHolder
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.ui.base.holder.BaseFlexibleViewHolder
-import kotlinx.android.synthetic.main.source_main_controller_card.title
+import eu.kanade.tachiyomi.databinding.SourceMainControllerCardHeaderBinding
 
 /**
  * Item that contains the selection header.
@@ -40,9 +40,11 @@ class SelectionHeader : AbstractHeaderItem<SelectionHeader.Holder>() {
         // Intentionally empty
     }
 
-    class Holder(view: View, adapter: FlexibleAdapter<IFlexible<RecyclerView.ViewHolder>>) : BaseFlexibleViewHolder(view, adapter) {
+    class Holder(view: View, adapter: FlexibleAdapter<*>) : FlexibleViewHolder(view, adapter) {
+        private val binding = SourceMainControllerCardHeaderBinding.bind(view)
+
         init {
-            title.text = view.context.getString(R.string.select_a_source_to_migrate_from)
+            binding.title.text = view.context.getString(R.string.select_a_source_to_migrate_from)
         }
     }
 

app/src/main/java/eu/kanade/tachiyomi/ui/recent/history/HistoryHolder.kt
@@ -2,20 +2,14 @@ package eu.kanade.tachiyomi.ui.recent.history
 
 import android.view.View
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import eu.davidea.viewholders.FlexibleViewHolder
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.data.database.models.MangaChapterHistory
 import eu.kanade.tachiyomi.data.glide.GlideApp
 import eu.kanade.tachiyomi.data.glide.toMangaThumbnail
-import eu.kanade.tachiyomi.ui.base.holder.BaseFlexibleViewHolder
+import eu.kanade.tachiyomi.databinding.HistoryItemBinding
 import eu.kanade.tachiyomi.util.lang.toTimestampString
 import java.util.Date
-import kotlinx.android.synthetic.main.history_item.cover
-import kotlinx.android.synthetic.main.history_item.holder
-import kotlinx.android.synthetic.main.history_item.last_read
-import kotlinx.android.synthetic.main.history_item.manga_source
-import kotlinx.android.synthetic.main.history_item.manga_title
-import kotlinx.android.synthetic.main.history_item.remove
-import kotlinx.android.synthetic.main.history_item.resume
 
 /**
  * Holder that contains recent manga item
@@ -29,18 +23,20 @@ import kotlinx.android.synthetic.main.history_item.resume
 class HistoryHolder(
     view: View,
     val adapter: HistoryAdapter
-) : BaseFlexibleViewHolder(view, adapter) {
+) : FlexibleViewHolder(view, adapter) {
+
+    private val binding = HistoryItemBinding.bind(view)
 
     init {
-        holder.setOnClickListener {
+        binding.holder.setOnClickListener {
             adapter.itemClickListener.onItemClick(bindingAdapterPosition)
         }
 
-        remove.setOnClickListener {
+        binding.remove.setOnClickListener {
             adapter.removeClickListener.onRemoveClick(bindingAdapterPosition)
         }
 
-        resume.setOnClickListener {
+        binding.resume.setOnClickListener {
             adapter.resumeClickListener.onResumeClick(bindingAdapterPosition)
         }
     }
@@ -55,22 +51,22 @@ class HistoryHolder(
         val (manga, chapter, history) = item
 
         // Set manga title
-        manga_title.text = manga.title
+        binding.mangaTitle.text = manga.title
 
         // Set source + chapter title
         val formattedNumber = adapter.decimalFormat.format(chapter.chapter_number.toDouble())
-        manga_source.text = itemView.context.getString(R.string.recent_manga_source)
+        binding.manga_source.text = itemView.context.getString(R.string.recent_manga_source)
             .format(adapter.sourceManager.getOrStub(manga.source).toString(), formattedNumber)
 
         // Set last read timestamp title
-        last_read.text = Date(history.last_read).toTimestampString()
+        binding.last_read.text = Date(history.last_read).toTimestampString()
 
         // Set cover
-        GlideApp.with(itemView.context).clear(cover)
+        GlideApp.with(itemView.context).clear(binding.cover)
         GlideApp.with(itemView.context)
             .load(manga.toMangaThumbnail())
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
             .centerCrop()
-            .into(cover)
+            .into(binding.cover)
     }
 }

app/src/main/java/eu/kanade/tachiyomi/ui/source/globalsearch/GlobalSearchCardHolder.kt
@@ -2,17 +2,17 @@ package eu.kanade.tachiyomi.ui.source.globalsearch
 
 import android.view.View
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import eu.davidea.viewholders.FlexibleViewHolder
 import eu.kanade.tachiyomi.data.database.models.Manga
 import eu.kanade.tachiyomi.data.glide.GlideApp
 import eu.kanade.tachiyomi.data.glide.toMangaThumbnail
-import eu.kanade.tachiyomi.ui.base.holder.BaseFlexibleViewHolder
+import eu.kanade.tachiyomi.databinding.GlobalSearchControllerCardItemBinding
 import eu.kanade.tachiyomi.widget.StateImageViewTarget
-import kotlinx.android.synthetic.main.global_search_controller_card_item.itemImage
-import kotlinx.android.synthetic.main.global_search_controller_card_item.progress
-import kotlinx.android.synthetic.main.global_search_controller_card_item.tvTitle
 
 class GlobalSearchCardHolder(view: View, adapter: GlobalSearchCardAdapter) :
-    BaseFlexibleViewHolder(view, adapter) {
+    FlexibleViewHolder(view, adapter) {
+
+    private val binding = GlobalSearchControllerCardItemBinding.bind(view)
 
     init {
         // Call onMangaClickListener when item is pressed.
@@ -32,15 +32,15 @@ class GlobalSearchCardHolder(view: View, adapter: GlobalSearchCardAdapter) :
     }
 
     fun bind(manga: Manga) {
-        tvTitle.text = manga.title
+        binding.tvTitle.text = manga.title
         // Set alpha of thumbnail.
-        itemImage.alpha = if (manga.favorite) 0.3f else 1.0f
+        binding.itemImage.alpha = if (manga.favorite) 0.3f else 1.0f
 
         setImage(manga)
     }
 
     fun setImage(manga: Manga) {
-        GlideApp.with(itemView.context).clear(itemImage)
+        GlideApp.with(itemView.context).clear(binding.itemImage)
         if (!manga.thumbnail_url.isNullOrEmpty()) {
             GlideApp.with(itemView.context)
                 .load(manga.toMangaThumbnail())
@@ -48,7 +48,7 @@ class GlobalSearchCardHolder(view: View, adapter: GlobalSearchCardAdapter) :
                 .centerCrop()
                 .skipMemoryCache(true)
                 .placeholder(android.R.color.transparent)
-                .into(StateImageViewTarget(itemImage, progress))
+                .into(StateImageViewTarget(binding.itemImage, binding.progress))
         }
     }
 }