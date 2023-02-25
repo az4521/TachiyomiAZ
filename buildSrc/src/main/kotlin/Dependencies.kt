2e80da9e953c16bbceefd1dab088a5ecdbf19c1f
az4521 <az4521@users.noreply.github.com>
2023-02-25 03:43:39 -0500

    Revert "bump some deps and break everything"
    This reverts commit 4e56ee4125f48af59ca3459efe1807edb52d8d1f.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,8 +3,8 @@ object Versions {
 }
 
 object BuildPluginsVersion {
-    const val AGP = "7.1.2"
-    const val KOTLIN = "1.8.10"
+    const val AGP = "4.0.1"
+    const val KOTLIN = "1.5.10"
     const val KTLINT = "9.2.1"
     const val VERSIONS_PLUGIN = "0.28.0"
 }
