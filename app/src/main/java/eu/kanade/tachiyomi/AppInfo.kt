2c3e7e9b4c749450b03bb648c17bce895d21c320
az4521 <18432684+az4521@users.noreply.github.com>
2024-12-17 22:42:53 +0000

    JSUT BUILD HGKAHJGHLG

--- app/src/main/java/eu/kanade/tachiyomi/AppInfo.kt
+++ app/src/main/java/eu/kanade/tachiyomi/AppInfo.kt
@@ -12,5 +12,6 @@ object AppInfo {
 
     fun getVersionName() = BuildConfig.VERSION_NAME
 
+    // @since extension-lib 1.5
     fun getSupportedImageMimeTypes() = ImageUtil.ImageType.entries.map { it.mime }
 }
